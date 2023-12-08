c959c1b88044c1b6cd7c16fa45238ba90d694091
eyemono-moe <ysdr83@gmail.com>
2023-12-09 03:34:27 +0900

    :sparkles: add modular forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,80 @@
+import {
+  SubmitHandler,
+  createForm,
+  getValues,
+  reset,
+} from "@modular-forms/solid";
+import { Show, createSignal } from "solid-js";
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
+
 function App() {
+  const [loginForm, { Form, Field }] = createForm<LoginForm>({
+    initialValues: {
+      email: "initial email",
+      password: "initial password",
+    },
+  });
+  const [showPassword, setShowPassword] = createSignal(false);
+
+  const handleSubmit: SubmitHandler<LoginForm> = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
+
+  const handleReset = () => {
+    reset(loginForm);
+  };
+
   return (
     <>
-      <h1>modular-forms-reset</h1>
+      <Form onSubmit={handleSubmit}>
+        <Field name="email">
+          {(field, props) => (
+            <label>
+              email
+              <input value={field.value || ""} {...props} type="email" />
+            </label>
+          )}
+        </Field>
+
+        <Show when={showPassword()}>
+          <Field name="password">
+            {(field, props) => (
+              <label>
+                password
+                <input value={field.value || ""} {...props} />
+              </label>
+            )}
+          </Field>
+        </Show>
+
+        <button onClick={handleReset} type="button">
+          Reset
+        </button>
+        <button type="submit">Submit</button>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={showPassword()}
+            onChange={(e) => setShowPassword(e.currentTarget.checked)}
+          />
+          show password
+        </label>
+      </Form>
+
+      <pre>
+        <code>
+          {JSON.stringify(
+            getValues(loginForm, { shouldActive: false }),
+            null,
+            2
+          )}
+        </code>
+      </pre>
     </>
   );
 }
